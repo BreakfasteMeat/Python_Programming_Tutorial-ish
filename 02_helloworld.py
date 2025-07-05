c517778fd3e7253fc4dbde7e4d5405f755d3af84
BreakfasteMeat <133184897+BreakfasteMeat@users.noreply.github.com>
2025-07-05 08:48:46 +0800

    yes

--- 02_helloworld.py
+++ 02_helloworld.py
@@ -1,14 +1,17 @@
 
-# In this script, There are three print statements one after the other
+# In this script, there are three print statements one after the other
 # The order they would appear in the console would start from the top till the very bottom
 
 print("Hello World!")
 
 print("Hello World 2!")
 
-# print("Hello World 3!")
+#print("Hello World 3!")
 
 print("Goodbye World")
 
 # It shows that code is run sequentially from TOP to BOTTOM (most of the time)
 # Notice how it also skips printing "Hello World 3!" as it is considered as a comment
+
+# A useful thing of note is that developers often "comment out" lines to temporarily disable them without deleting the code
+# Try uncommenting the aforementioned "Hello World 3!" by removing the pound (#) symbol
